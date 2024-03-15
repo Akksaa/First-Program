30425cda1b2e30539aa0e28ab580fec50e55cb52
Akksaa <shireensaeed786@gmail.com>
2024-03-15 19:46:00 +0500

    files added

---
+++ firstProgram.js
@@ -0,0 +1,14 @@
+"use strict";
+let winningNumber = 120;
+let userGuess = prompt("Guess a number");
+if (userGuess == winningNumber) {
+    console.log("Your guess is Right!!");
+}
+else {
+    if (userGuess > winningNumber) {
+        console.log(" too high!, Try a small number.");
+    }
+    else {
+        console.log("too low!, Try a large number. ");
+    }
+}
---
+++ firstProgram.ts
@@ -0,0 +1,13 @@
+let winningNumber : any = 120;
+let userGuess :any = prompt("Guess a number");
+if (userGuess == winningNumber) {
+    console.log("Your guess is Right!!");
+}
+else {
+    if (userGuess > winningNumber) {
+        console.log(" too high!, Try a small number.");
+    }
+    else {
+        console.log("too low!, Try a large number. ");
+    }
+}
